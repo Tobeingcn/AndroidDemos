5c2d49aaa6d5abbacca7bf6f8972237ee25c6dc1
sunzheng <sunzheng123pr@gmail.com>
2015-06-04 16:07:41 +0800

    add files

--- TobeingTest/src/com/tobeing/test/ui/ScanActivity.java
+++ TobeingTest/src/com/tobeing/test/ui/ScanActivity.java
@@ -2,6 +2,7 @@
 package com.tobeing.test.ui;
 
 import com.tobeing.test.R;
+import com.tobeing.test.util.TBLog;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.View.OnClickListener;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class ScanActivity extends Activity {
     
@@ -36,13 +38,32 @@ public class ScanActivity extends Activity {
             
             @Override
             public void run() {
+                String path="/storage/emulated/0/Music/歌曲识别测试/多个歌手";
+                path="/storage/extSdCard/extMusic/歌单";
+                String lastStream="";
+                int i=0;
+                while (i++<1000) {
+                    try {
+                        Thread.sleep(100);
+                        File[] files=new File(path).listFiles();
+                        String newlist=Arrays.toString(files);
+                        TBLog.d("newlist", "newlist="+newlist);
+                        if(lastStream!=null&&!lastStream.equals(newlist)){
+                            new IllegalArgumentException("error different ");
+                        }else{
+                            lastStream=newlist;
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
                 //printAllSdcard(Environment.getExternalStorageDirectory(), 0);
-                printAllSdcard(new File("/storage/emulated/legacy"), 0); 
-                printAllSdcard(new File("/storage/emulated/0"), 0); 
-                printAllSdcard(new File("/storage/extSdCard"), 0); 
-                printAllSdcard(new File("/mnt/shell/emulated"), 0);
-                printAllSdcard(new File("/storage/emulated"), 0); 
-                printAllSdcard(new File("/sdcard/"), 0); 
+                //printAllSdcard(new File("/storage/emulated/legacy"), 0); 
+                //printAllSdcard(new File("/storage/emulated/0"), 0); 
+                //printAllSdcard(new File("/storage/extSdCard"), 0); 
+                //printAllSdcard(new File("/mnt/shell/emulated"), 0);
+                //printAllSdcard(new File("/storage/emulated"), 0); 
+                //printAllSdcard(new File("/sdcard/"), 0); 
                // printAllSdcard(new File("/storage/emulated/0/tencent/tassistant/log/"), 0);
             }
         }).start();
